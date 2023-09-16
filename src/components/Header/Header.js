abc6af20fc07b519a1805d429144498e9b7703f8
Marina Malyarchuk <pchupchu@yandex.ru>
2023-09-16 15:24:26 +0300

    Add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,17 @@
-import './Header.css'
+import './Header.css';
+import logo from '../../images/logo.svg'
 
 function Header() {
   return (
     <div className="header">
-      <p>
-      Features
-  Team
-  Sign In
-      </p>
+      <img className="header__logo" src={logo} alt="logo" />
+      <nav className='header__navigation'>
+        <ul className='header__links'>
+          <li><a className='header__link' href=' '>Features</a></li>
+          <li><a className='header__link' href=' '>Team</a></li>
+          <li><a className='header__link' href=' '>Sign In</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
