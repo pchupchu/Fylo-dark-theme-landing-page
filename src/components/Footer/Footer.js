4910653139278f95d8c45318d1d9c53fc714c4a7
Marina Malyarchuk <pchupchu@yandex.ru>
2023-09-22 16:37:05 +0300

    Fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,70 +4,59 @@ import logo from '../../images/logo.svg'
 function Footer() {
   return (
     <footer className="footer">
-      <img className="footer__logo" src={logo} alt="logo" />
-      <div className="footer__container">
-        <div className='footer__contacts'>
-          <div className='footer__icon footer__icon_address'></div>
-          <p className='footer__text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-  dolore magna aliqua</p>
-          <div className='footer__icon footer__icon_phone'></div>
-          <p className='footer__text'>+1-543-123-4567</p>
-          <div className='footer__icon footer__icon_email'></div>
-          <p className='footer__text'>example@fylo.com</p>
-        </div>
-        <nav className='footer__navigation'>
-          <ul className='footer__links'>
+      <div className='footer__content'>
+        <img className="footer__logo" src={logo} alt="logo" />
+        <div className="footer__container">
+          <div className='footer__contacts'>
+            <div className='footer__icon footer__icon_address'>
+              <p className='footer__text footer__text_contact footer__text_address'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+            </div>
+            <div className='footer__icon footer__icon_phone'>
+              <p className='footer__text footer__text_contact'>+1-543-123-4567</p>
+            </div>
+            <div className='footer__icon footer__icon_email'>
+              <p className='footer__text footer__text_contact'>example@fylo.com</p>
+            </div>
+          </div>
+          <nav className='footer__navigation'>
+            <ul className='footer__links'>
+                <li>
+                  <a className='footer__text footer__link' href=' '>About Us</a>
+                </li>
+                <li>
+                  <a className='footer__text footer__link' href=' '>Jobs</a>
+                </li>
+                <li>
+                  <a className='footer__text footer__link' href=' '>Press</a>
+                </li>
+                <li>
+                  <a className='footer__text footer__link' href=' '>Blog</a>
+                </li>
+            </ul>
+            <ul className='footer__links'>
               <li>
-                <a className='footer__text footer__link' href=' '>About Us</a>
+                <a className='footer__text footer__link' href=' '>Contact Us</a>
               </li>
               <li>
-                <a className='footer__text footer__link' href=' '>Jobs</a>
+                <a className='footer__text footer__link' href=' '>Terms</a>
               </li>
               <li>
-                <a className='footer__text footer__link' href=' '>Press</a>
+                <a className='footer__text footer__link' href=' '>Privacy</a>
+              </li>
+            </ul>
+            <ul className='footer__links  footer__social-icons'>
+              <li>
+              <a className='footer__social-icon footer__social-icon_facebook' href='https://www.facebook.com/'> </a>
               </li>
               <li>
-                <a className='footer__text footer__link' href=' '>Blog</a>
+              <a className='footer__social-icon footer__social-icon_twitter' href='https://twitter.com/'> </a>
               </li>
-          </ul>
-          <ul className='footer__links'>
-            <li>
-              <a className='footer__text footer__link' href=' '>Contact Us</a>
-            </li>
-            <li>
-              <a className='footer__text footer__link' href=' '>Terms</a>
-            </li>
-            <li>
-              <a className='footer__text footer__link' href=' '>Privacy</a>
-            </li>
-          </ul>
-        </nav>
-        {/* <div>
-          <ul className='footer__links'>
-            <li>
-              <a className='footer__text footer__link' href=' '>Contact Us</a>
-            </li>
-            <li>
-              <a className='footer__text footer__link' href=' '>Terms</a>
-            </li>
-            <li>
-              <a className='footer__text footer__link' href=' '>Privacy</a>
-            </li>
-          </ul>
-        </div> */}
-        <div>
-          <ul className='footer__links  footer__social-icons'>
-            <li>
-            <a className='footer__social-icon footer__social-icon_facebook' href='https://www.facebook.com/'> </a>
-            </li>
-            <li>
-            <a className='footer__social-icon footer__social-icon_twitter' href='https://twitter.com/'> </a>
-            </li>
-            <li>
-            <a className='footer__social-icon footer__social-icon_instagram' href='https://www.instagram.com/'>
-            </a>
-            </li>
-          </ul>
+              <li>
+              <a className='footer__social-icon footer__social-icon_instagram' href='https://www.instagram.com/'>
+              </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
       
